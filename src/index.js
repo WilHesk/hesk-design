7a66c9f50b8d79ae0c33e365d7baaaf6834f9bb0
Wil Hesk <wilhesk@gmail.com>
2021-05-24 16:21:42 +0100

    rebuild
    straight build cra

--- src/index.js
+++ src/index.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
 
-const myfirstelement = <h1>Hello React!</h1>
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
 
-ReactDOM.render(myfirstelement, document.getElementById('root'));
-
-/*
-You are now watching a React file
-through our 'Show React' tool.
-Click the tab to check out
-the 'index.html' file as well.
-*/
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
